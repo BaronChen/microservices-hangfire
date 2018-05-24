91cabd41be961acf06714d7804b5242bbca8e639
baron <baron.zhongyangchen@gmail.com>
2018-05-24 23:23:03 +1000

    Fixing up linux test script

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 dotnet test ./E8ay.Common.HangFire.Test/E8ay.Common.HangFire.Test.csproj
 dotnet test ./E8ay.Item.Services.Test/E8ay.Item.Services.Test.csproj
 dotnet test ./E8ay.Bid.Services.Test/E8ay.Bid.Services.Test.csproj
